11d26fd26402d81344e58755d1ca2378b3540f64
jeanbeanie <msjeanneramos@gmail.com>
2020-09-23 23:00:22 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
