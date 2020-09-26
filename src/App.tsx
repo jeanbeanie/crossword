79ff57d5fac6acb7e7974a4e69b08a311c84fad3
jeanbeanie <msjeanneramos@gmail.com>
2020-09-26 21:17:18 +0000

    added more logic for crossword grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const StyledTile = styled.div`
+	border: 1px solid black;
+	height: ${(props: {width:number}) => props.width}px;
+	width: ${props => props.width}px;
+	float: left;
+	background-color: white;
+`
+
+const StyledGrid = styled.div`
+	max-width: 420px;
+	min-height: 420px;
+	border: 6px solid lightblue;
+`
+
+function Tile(props:{id: number}) {
+	return (
+		<StyledTile width={tileWidth}>{props.id}</StyledTile>
+	)
+}
+
+const gridWidth = 420;
+const tileWidth = 40;
+
+function Grid(props:{width:number}) {
+	const trueTileWidth = 40+2; // tile width plus border edges
+	const numTilesAcross = props.width/trueTileWidth;
+	
+	const createGrid = function():JSX.Element[][]{
+		const grid = [];
+		for(let i=0;i<numTilesAcross;i++){
+			const row  = [];
+			for(let j=0;j<numTilesAcross;j++){
+				const id = parseInt(`${i}${j}`)
+				row.push(<Tile id={id} />);
+			}
+			grid.push(row);
+		}
+		return grid;
+	}
+	return (
+		<StyledGrid>
+			{createGrid()}
+		</StyledGrid>
+	);
+}
 
 function App() {
   return (
     <div id="App">
-    	<h1>My App</h1>
+    	<h1>Crossword</h1>
+	<Grid width={gridWidth}/>
 	</div>
   );
 }
