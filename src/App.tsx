f4e6e8357e1934d08c37f5e21bfb4781b0933063
jeanbeanie <msjeanneramos@gmail.com>
2020-09-27 04:30:07 +0000

    added logic for black blocks and horizontal word mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import styled from 'styled-components';
 
@@ -27,30 +27,71 @@ function Tile(props:{id: number, color:string, tileLetter:string}) {
 	)
 }
 
-const wordBank = ["ba", "bar",]
+//const wordBank = ["ba", "bar",]
 
+interface TileProps {
+  isBlocked: boolean,
+  id: number,
+  color: string,
+  tileLetter:string,
+  key: number,
+}
+
+const horizontalWordMap:number[] = [];
 function Grid(props:{width:number}) {
 	const trueTileWidth = tileWidth+2; // tile width plus border edges
 	const numTilesAcross = props.width/trueTileWidth;
+  
+  //const [horizontalWordMap, setHorizontalWordMap] = useState<any>([]);   
+
+  const isTileStartOfWord = (id:number,i:number,j:number,prevTileIsBlocked:boolean) => {
+    if(id === 0){
+    console.log('in 1')
+      return true;
+    }
+    else if (j === 0 && !prevTileIsBlocked) {
+    console.log('in 2')
+      return true;
+    } else {
+     return prevTileIsBlocked
+    }
+  }
 	
 	const createGrid = function():JSX.Element[][]{
-		const grid = [];
+		const grid:TileProps[][] = [];
+
 		for(let i=0;i<numTilesAcross;i++){
 			const row  = [];
 			for(let j=0;j<numTilesAcross;j++){
 				const id = parseInt(`${i}${j}`);
-        const blocked = i+2===j || i+6===j || j+2===i || j+6===i // todo figure out patterns
-        const color = blocked ? "black" : "white";
-				row.push(<Tile id={id} color={color} tileLetter={""}/>); //todo tileLetter func
+        const isBlocked = i+2===j || i+6===j || j+2===i || j+6===i // todo figure out patterns
+        const color = isBlocked ? "black" : "white";
+				row.push({id, color, tileLetter:"", isBlocked, key:id}); //todo tileLetter func
 			}
 			grid.push(row);
 		}
-		return addWordsToGrid(grid);
+		return addTilesToGrid(grid);
 	}
 
-  const addWordsToGrid = (grid:JSX.Element[][]) => {
-    //
-    return grid;
+  const addTilesToGrid = (grid:TileProps[][]) => {
+    const tileGrid = [];
+		for(let i=0;i<numTilesAcross;i++){
+			const row  = [];
+			for(let j=0;j<numTilesAcross;j++){
+        const tileProps = grid[i][j]
+        const isBlocked = j === 0 ? true : grid[i][j-1].isBlocked
+        const isStartOfWord = isTileStartOfWord(tileProps.id,i,j,isBlocked);
+        //const horizontal = isStartOfWord ? [...horizontalWordMap, tileProps.id] : horizontalWordMap;
+        if(isStartOfWord){
+          horizontalWordMap.push(tileProps.id);
+          console.log("HWM", horizontalWordMap)
+        }
+	      console.log(isStartOfWord, tileProps.key)		
+        row.push(<Tile {...tileProps}/>); //todo tileLetter func 
+      }
+      tileGrid.push(row);
+    }
+    return tileGrid;
   };
 
 	return (
@@ -61,6 +102,7 @@ function Grid(props:{width:number}) {
 }
 
 function App() {
+
   return (
     <div id="App">
 	<h1>Crossword</h1>
