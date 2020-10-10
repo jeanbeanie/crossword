4da61212cd5fab5bb4f92060120e441c4898f229
jeanbeanie <msjeanneramos@gmail.com>
2020-10-10 03:17:53 +0000

    more type fixing, cleanup, and refactoring

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-// import './../App.css';
 import styled from 'styled-components';
 
 
@@ -20,13 +19,15 @@ interface TileProps {
   isBlocked: boolean;
   id: number;
   color: string;
-  tileLetter: string;
+  displayLetter: string;
   key?: number;
+  width: number;
 }
 
 function Tile(props: TileProps): JSX.Element {
+  const {width, color, displayLetter, id} = props;
 	return (
-		<StyledTile width={TILE_WIDTH} color={props.color}>{props.id}{props.tileLetter}</StyledTile>
+		<StyledTile width={width} color={color}> {id}{displayLetter} </StyledTile>
   );
 }
 
@@ -35,23 +36,25 @@ function Tile(props: TileProps): JSX.Element {
 
 /*** GRID COMPONENTS ***/
 
-interface GridProps { width: number }
+interface GridProps { 
+  width: number;
+  tileWidth: number;
+  numTilesAcross: number;
+}
 interface GridState {
   tileMap: JSX.Element[][];
   horizontalWordMap: number[];
   verticalWordMap: number[];
 }
 
-const StyledGrid = styled.div`
-	max-width: ${GRID_WIDTH}px;
-	min-height: ${GRID_WIDTH}px;
+const StyledGrid = styled.div<{width: number}>`
+  max-width: ${(props): number => props.width}px;
+  min-height: ${(props): number => props.width}px;
 	border: 6px solid lightblue;
 `;
     
 function Grid(props: GridProps): JSX.Element {
-  const trueTileWidth = (TILE_WIDTH + 2); // tile width plus border edges
-	const numTilesAcross = props.width/trueTileWidth;
-
+  // TODO these 2 funcs can be put together somehow, if it won't be too confusing
   const tileStartsHorizontalWord = (tile: TileProps, colIndex: number, leftTileIsBlocked: boolean): boolean => {
     if(colIndex === 0 && !tile.isBlocked){
       return true;
@@ -68,24 +71,21 @@ function Grid(props: GridProps): JSX.Element {
     }
   }
 
-  const addTilesToGrid = (grid: TileProps[][]): GridState => {
-    const tileMap = [];
-    const horizontalWordMap = [];
-    const verticalWordMap = [];
+  const buildStateFromGrid = (grid: TileProps[][]): GridState => {
+    const tileMap: JSX.Element[][] = [];
+    const horizontalWordMap: number[] = [];
+    const verticalWordMap: number[] = [];
 
     // build Grid rows
-    for(let rowIndex=0; rowIndex<numTilesAcross; rowIndex++){
-      const row  = [];
+    for(let rowIndex=0; rowIndex<props.numTilesAcross; rowIndex++){
+      const row: JSX.Element[] = [];
 
       // build Grid columns
-			for(let colIndex=0; colIndex<numTilesAcross; colIndex++){
+			for(let colIndex=0; colIndex<props.numTilesAcross; colIndex++){
         const tileProps = grid[rowIndex] ? grid[rowIndex][colIndex] : false;
         if(tileProps){
           const isLeftBlocked = colIndex === 0 ? true : grid[rowIndex][colIndex-1].isBlocked
           const isUpperBlocked = rowIndex === 0 ? true : grid[rowIndex-1][colIndex].isBlocked;
-          // console.log("grid[rowIndex][colIndex]", grid[rowIndex][colIndex], "row", rowIndex, "col", colIndex) 
-          // console.log("isLeftBlocked", isLeftBlocked);
-          // console.log("isUpperBlocked", isUpperBlocked);
           const startsHorizontalWord = tileStartsHorizontalWord(tileProps, colIndex, isLeftBlocked);
           const startsVerticalWord = tileStartsVerticalWord(tileProps, rowIndex, isUpperBlocked)
           
@@ -99,33 +99,34 @@ function Grid(props: GridProps): JSX.Element {
       }
       tileMap.push(row);
     }
-    console.log( {tileMap, horizontalWordMap, verticalWordMap} );
     return {tileMap, horizontalWordMap, verticalWordMap}
   };
   
 
-	const createGridState = function(): GridState {
+	const returnState = function(): GridState {
     const grid: TileProps[][] = [];
-		for(let i=0;i<numTilesAcross;i++){
-			const row  = [];
-			for(let j=0;j<numTilesAcross;j++){
+    const {numTilesAcross, tileWidth} = props;
+
+		for(let i=0; i<numTilesAcross; i++){
+			const row: TileProps[] = [];
+			for(let j=0; j<numTilesAcross; j++){
 				const id = parseInt(`${i}${j}`);
         const isBlocked = i+2===j || i+6===j || j+2===i || j+6===i // todo figure out other patterns
         const color = isBlocked ? "black" : "white";
-				row.push({id, color, tileLetter:"", isBlocked, key:id}); //todo tileLetter func here?
+        const width = tileWidth;
+
+				row.push({width, id, color, displayLetter:"", isBlocked, key:id}); //todo tileLetter func here?
 			}
 			grid.push(row);
 		}
-    const tileGrid = addTilesToGrid(grid)
-    return tileGrid
+    return buildStateFromGrid(grid);
 	}
 
-  // TODO set setState 
-  const [state] = useState<GridState>(createGridState());
+  const [state] = useState<GridState>(returnState());
 
   return (
     <React.Fragment>
-      <StyledGrid>
+      <StyledGrid width={props.width}>
         {state.tileMap}
       </StyledGrid>
     </React.Fragment>
@@ -135,10 +136,13 @@ function Grid(props: GridProps): JSX.Element {
 /*** END OF GRID COMPONENTS ***/
 
 function App(): JSX.Element {
+  const trueTileWidth = TILE_WIDTH + 2; // tile width plus border edges
+	const numTilesAcross = GRID_WIDTH/trueTileWidth;
+
   return (
     <div id="App">
       <h1>Crossword</h1>
-      <Grid width={GRID_WIDTH}/>
+      <Grid width={GRID_WIDTH} tileWidth={TILE_WIDTH} numTilesAcross={numTilesAcross}/>
     </div>
   );
 }
